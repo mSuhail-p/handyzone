789dfe20f7c47acb707ec31409e0be1043fdd6bc
muhammedSuhail <suhailkzp1@gmail.com>
2025-03-28 10:59:31 +0530

    add password hashing with bcrypt package

--- backend/src/services/userServices.ts
+++ backend/src/services/userServices.ts
@@ -1,8 +1,14 @@
+import bcrypt from "bcryptjs";
 import { userRepository } from "../Repository/userRepository";
+
 const userRepo = new userRepository();
+
 export class userSercies {
   async signupUser(userData: any) {
     try {
+      const hashedPassword = await this.hashPassword(userData.password);
+      userData.password = hashedPassword;
+
       const user = await userRepo.signupUser(userData);
       return user;
     } catch (err) {
@@ -10,4 +16,9 @@ export class userSercies {
       throw new Error("Error saving user data:" + err);
     }
   }
+
+  hashPassword = async (password: string) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return hashedPassword;
+  };
 }
